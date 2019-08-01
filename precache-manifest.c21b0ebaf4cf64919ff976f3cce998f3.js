b00e0824815396f2ca75f6c22f73481f77cd8c40
AsadM Muaviya <asadmuaviya.ch@gmail.com>
2019-08-01 19:23:17 +0500

    my first app on github
    wow its great plateform

---
+++ precache-manifest.c21b0ebaf4cf64919ff976f3cce998f3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58bcf14fbd125f54ccb22d54af2a184f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4462bf3ff86969e61eff",
+    "url": "/static/css/main.835ecee3.chunk.css"
+  },
+  {
+    "revision": "99398fb0fd888992ba9a",
+    "url": "/static/js/2.727932f2.chunk.js"
+  },
+  {
+    "revision": "4462bf3ff86969e61eff",
+    "url": "/static/js/main.57bb66b5.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
